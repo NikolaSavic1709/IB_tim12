9f542a5d77ea20e0ec6ba4180d55fa12771320d3
Nikola1709 <savickosovska@gmail.com>
2023-03-27 19:25:11 +0200

    Spring boot project initialized

---
+++ back/ib/src/main/java/com/ib/IbApplication.java
@@ -0,0 +1,13 @@
+package com.ib;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IbApplication.class, args);
+	}
+
+}
---
+++ back/ib/src/test/java/com/ib/IbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ib;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
